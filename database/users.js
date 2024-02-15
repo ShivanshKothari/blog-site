b1e721a8783f0a8e6f4c005b5363150ac1cb9bc4
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-02-15 11:05:40 +0530

    Updated users schema

--- database/users.js
+++ database/users.js
@@ -1,12 +1,13 @@
 // models/users.js
 import mongoose from 'mongoose';
 
-const blogSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: String,
-  password: String,
+  email: String,
+  password: String
   // Add other fields as needed
 });
 
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = mongoose.model('users', userSchema);
 
 export default Blog;
