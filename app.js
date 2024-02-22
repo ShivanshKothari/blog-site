ce8d287f13d2cdbfba20ce06b2a58206accffb05
ShivanshKothari <shivanshkothari2004@gmail.com>
2024-02-22 18:10:21 +0530

    debug/session-management-over-https

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(
       maxAge: 2 * 24 * 60 * 60 * 1000,
       secure: !(process.env.NODE_ENV !== 'production'),
       sameSite: 'none',
-      httpOnly: true,
+      // httpOnly: true,
       path: '/'
     },
   })
